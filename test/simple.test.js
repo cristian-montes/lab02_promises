437b3482321b7a12c71e32a029a18c2db0bdd3d5
Cristian Montes <cmontesmujica@gmail.com>
2021-09-17 18:06:07 -0700

    almost al test passing

--- test/simple.test.js
+++ test/simple.test.js
@@ -1,5 +1,6 @@
+import exp from 'constants';
 import { rm, mkdir } from 'fs/promises';
-import { get } from 'http';
+// import { get } from 'http';
 import { SimpleDb } from '../simple-db.js';
 
 describe('simple data structure', () => {
@@ -11,7 +12,7 @@ describe('simple data structure', () => {
         return mkdir(rootDir, { recursive:true });
       });
   });
-
+  // 1  ------------------------------------------------//
   it('it should save objects', () => {
     const simpleDB = new SimpleDb(rootDir);
 
@@ -26,7 +27,7 @@ describe('simple data structure', () => {
         expect(data.id).toEqual(expect.any(String)); // FIGURE OUT HOW TO COMPARE FAKE TO REAL
       });
   });
-
+  // 2------------------------------------------------//
   it('it gets objects by id', () => {
     const simpleDB = new SimpleDb(rootDir);
 
@@ -36,8 +37,8 @@ describe('simple data structure', () => {
     };
 
     return simpleDB
-      .save(data).then(() => { 
-        return simpleDB.get().then((result) => {
+      .save(data).then((id) => { 
+        return simpleDB.get(id).then((result) => {
           expect(result).toEqual({ a:'a',
             b:'b', 
             id: expect.any(String)
@@ -45,4 +46,91 @@ describe('simple data structure', () => {
         });
       });
   });
+  
+  // 3------------------------------------------------//
+
+  it('it gets objects by id nul', () => {
+    const simpleDB = new SimpleDb(rootDir);
+
+    const  data = {
+      a:'a',
+      b:'b'
+    };
+    
+
+    return simpleDB
+      .save(data).then(() => { 
+        return simpleDB.get().then((result) => {
+          expect(result).toEqual(null);
+        });
+      });
+  });
+
+  // 4------------------------------------------------//
+  it('it get all objects', () => {
+    const simpleDB = new SimpleDb(rootDir);
+    // const simpleDB2 = new SimpleDb(rootDir);
+
+    const  data = {
+      a:'a',
+      b:'b'
+    };
+    
+
+    return simpleDB
+      .save(data).then(() => { 
+        return simpleDB.getAll().then((result) => {
+          expect(result).toEqual([
+            { a:'a',
+              b:'b', 
+              id: expect.any(String)
+            }
+          ]);
+        });
+      });
+  });
+
+  // 5------------------------------------------------//
+  it('it deletes an object by id', () => {
+    const simpleDB = new SimpleDb(rootDir);
+
+    const  data = {
+      a:'a',
+      b:'b'
+    };
+    
+    return simpleDB
+      .save(data).then((id) => { 
+        return simpleDB.remove(id).then((res) => {
+          return simpleDB.get(res).then((obj) => {
+            expect(obj).toEqual(null);
+          });
+        });
+  
+      });
+  });
+
+  // 6 ------------------------------------------------//
+  it('update an obj', () => {
+    const simpleDB = new SimpleDb(rootDir);
+
+    const  data = {
+      a:'a',
+      b:'b'
+    };
+
+    return simpleDB
+      .save(data).then((id) => {
+        simpleDB.update(id, 'gooy').then((obj) => {
+          expect(obj).toEqual(
+            {
+              id: expect.any(String),
+              a:'goofy',
+              b:'b'
+            });
+        });
+      });
+  });
+
+  // ------------------------------------------------//
 });

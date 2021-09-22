d437aebc1f8ae122640fc36edf9b3d73dea5a47e
Cristian Montes <cmontesmujica@gmail.com>
2021-09-21 17:30:23 -0700

    fixing CI error

--- simple.js
+++ simple.js
@@ -1,9 +1,7 @@
 import { mkdir }   from 'fs/promises';
-// import { SimpleDb } from './simple-db';
 
 export const makeFoulder = (destination) => {
   return mkdir(destination)
     .then(() => {
-      return console.log(destination);
     });
 };
